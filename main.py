f58696e1e4850d2de5ca7ea04d5f31b869618693
MarcosInfantino <marcosinfantino698@gmail.com>
2022-05-22 16:22:07 -0300

    avanzamos un poco

--- main.py
+++ main.py
@@ -1,1 +1,122 @@
-print("Para mi eso es una paloma")
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import matplotlib.pyplot as plt
+
+image_size = (224, 224)
+batch_size = 32
+
+
+def main():
+    train_ds, val_ds = preprocess_data()
+    # input_sape es el formato de la imagen de entrada
+    # num_classes es la cantidad de pajaros que vamos a analizar,
+    model = make_model(input_shape=image_size + (3,), num_classes=5)
+    keras.utils.plot_model(model, show_shapes=True)
+    train_model(model, train_ds, val_ds)
+
+
+def preprocess_data():
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        "dataset/train",
+        validation_split=0.2,
+        subset="training",
+        seed=1337,
+        image_size=image_size,
+        batch_size=batch_size,
+    )
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        "dataset/valid",
+        validation_split=0.2,
+        subset="validation",
+        seed=1337,
+        image_size=image_size,
+        batch_size=batch_size,
+    )
+
+    plt.figure(figsize=(10, 10))
+    for images, labels in train_ds.take(1):
+        for i in range(9):
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.show()
+            plt.title(int(labels[i]))
+            plt.axis("off")
+
+    return train_ds, val_ds
+
+
+def make_model(input_shape, num_classes):
+    data_augmentation = keras.Sequential(
+        [
+            layers.RandomFlip("horizontal"),
+            layers.RandomRotation(0.1),
+        ]
+    )
+    inputs = keras.Input(shape=input_shape)
+    x = data_augmentation(inputs)
+
+    # Entry block
+    x = layers.Rescaling(1.0 / 255)(x) # Pasa de un rango de [0,255] a [0,1]
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("sigmoid")(x)
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+
+
+def train_model(model, train_ds, val_ds):
+    epochs = 50
+
+    callbacks = [
+        keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+    ]
+    model.compile(
+        optimizer=keras.optimizers.Adam(1e-3),
+        loss="binary_crossentropy",
+        metrics=["accuracy"],
+    )
+    model.fit(
+        train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+    )
+
+
+if __name__ == '__main__':
+    main()
+    print("FIN")
